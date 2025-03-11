d485a121b94e65a4290128f8dbbd1ca2ce782f34
nazmulpasetti <nazmulpasetti@gmail.com>
2025-03-11 23:52:32 +0000

    Add 6.js

---
+++ src/6.js
@@ -0,0 +1,5 @@
+  function getRandomColor() {
+    var colors = ["Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Violet"];
+    return colors[Math.floor(Math.random() * colors.length)];
+  }
+  
