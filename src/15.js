7ad188e65f3ebb0a9c0f7248c436a42858706822
nazmulpasetti <nazmulpasetti@gmail.com>
2025-03-20 05:18:38 +0000

    Add 15.js

---
+++ src/15.js
@@ -0,0 +1,20 @@
+// This is a JavaScript file
+
+// The following function takes in two numbers and returns their sum
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+// The following function takes in three numbers and returns their product
+function multiply(num1, num2, num3) {
+    return num1 * num2 * num3;
+}
+
+// The following function takes in a number and returns its square
+function square(num) {
+    return Math.pow(num, 2);
+}
+
+console.log(add(3, 4)); // Output: 7
+console.log(multiply(3, 4, 5)); // Output: 60
+console.log(square(10)); // Output: 100
